060b8a48432f11708ff7e9cbe8fcefc7636db061
MeHighLove <mikhailov_danya@mail.ru>
2021-02-14 14:53:49 +0300

    delete try catch & add correct tests & customize prettier

--- source/zip.js
+++ source/zip.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /**
  * Accepts several objects as input and returns a single object containing all fields from all objects are valid.
@@ -8,18 +8,15 @@
  * // returns {example: ex, exm: e}
  * zip({example: ex}, {exm: e})
  * @arg {...object} objs - Objects to merge
- * @returns {object | null} Returns null if some argument is not an Object
+ * @throws {TypeError} - Throws an error if some argument is not an object
+ * @returns {object}
  */
 
 const zip = (...objs) => {
-  try {
     objs.forEach((element) => {
-      if (Object.prototype.toString.call(element) !== "[object Object]") {
-        throw new Error("Some argument is not an Object!"); //Для undefined и null прокинется ошибка во время вызова constructor
-      }
+        if (Object.prototype.toString.call(element) !== '[object Object]') {
+            throw new TypeError('Some argument is not an Object!'); //Для undefined и null прокинется ошибка во время вызова constructor
+        }
     });
-  } catch (e) {
-    return null;
-  }
-  return Object.assign({}, ...objs.reverse());
+    return Object.assign({}, ...objs.reverse());
 };
